55ae2de64f7a072628c3d74523568bc87a672867
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2016-09-13 17:59:27 -0400

    Add yelp client.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const Yelp = require('yelp');
 
 router.get('/', function (req, res) {
   res.render('index', { title: 'Where Should I Eat?' });
 });
 
 router.get('/eat', function (req, res) {
-  console.log(req.query);
-  res.json('ok');
-});
+  let yelp = new Yelp({
+    consumer_key: process.env.YELP_CONSUMER_KEY,
+    consumer_secret: process.env.YELP_CONSUMER_SECRET,
+    token: process.env.YELP_TOKEN,
+    token_secret: process.env.YELP_TOKEN_SECRET
+  });
 
+  yelp.search({
+    location: req.query.location,
+    sort: 2, // 2 = rating
+    limit: 20
+  }).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
