2ec6b06352e9d09a6c41a4da6ea2045b02b70878
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2016-09-14 00:24:25 -0400

    Work on finder.

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,19 @@
 
 const express = require('express');
 const router = express.Router();
-const Yelp = require('yelp');
+
+const finder = require('../lib/finder');
 
 router.get('/', function (req, res) {
   res.render('index', { title: 'Where Should I Eat?' });
 });
 
 router.get('/eat', function (req, res) {
-  let yelp = new Yelp({
-    consumer_key: process.env.YELP_CONSUMER_KEY,
-    consumer_secret: process.env.YELP_CONSUMER_SECRET,
-    token: process.env.YELP_TOKEN,
-    token_secret: process.env.YELP_TOKEN_SECRET
-  });
-
-  yelp.search({
-    location: req.query.location,
-    sort: 2, // 2 = rating
-    limit: 20
-  }).then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.status(500).json(err);
-  });
+  finder.findRestaurant(req).then((restaurant) => {
+      res.render('eat', {restaurant: restaurant});
+    }).catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
