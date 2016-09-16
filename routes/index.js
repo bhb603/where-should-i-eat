244a525f998e814ac24e28326066452c6306abfd
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2016-09-16 09:05:40 -0400

    Add save params feature.

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,44 @@ const router = express.Router();
 const url = require('url');
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Where Should I Eat?' });
+  let savedParams = req.cookies.wsie_params;
+  res.render('index', {savedParams: savedParams});
 });
 
 router.get('/eat', function (req, res) {
   finder.findRestaurant(req).then((data) => {
-      if (req.query.format === 'json') {
-        return res.json(data);
-      }
+    if (req.query.save === 'true') {
+     res.cookie('wsie_params', {
+       location: req.query.location,
+       radius: req.query.radius,
+       search: req.query.search
+     }, {
+       maxAge: 2592000000 // 30 days
+     });
+    } else {
+      res.clearCookie('wsie_params');
+    }
 
-      let mapUrl = '';
-      if (data) {
-        mapUrl =
-          'https://www.google.com/maps?q=' +
-          data.location.address.join(' ') +
-          data.location.city + ', ' +
-          data.location.state_code + ' ' +
-          data.location.postal_code.replace(/\s+/g, '+');
-      }
+    if (req.query.format === 'json') {
+      return res.json(data || {});
+    }
 
-      let fullUrl = req.originalUrl;
-      let url = `/eat?location=${req.query.location}&radius=${req.query.radius}`;
-      res.render('eat', {data: data, fullUrl: fullUrl, url: url, mapUrl: mapUrl});
-    }).catch((err) => {
-      res.status(500).json(err);
-    });
+    let mapUrl = '';
+    if (data) {
+      mapUrl =
+        'https://www.google.com/maps?q=' +
+        data.location.address.join(' ') +
+        data.location.city + ', ' +
+        data.location.state_code + ' ' +
+        data.location.postal_code.replace(/\s+/g, '+');
+    }
+
+    let fullUrl = req.originalUrl;
+    let url = `/eat?location=${req.query.location}&radius=${req.query.radius}`;
+    res.render('eat', {data: data, fullUrl: fullUrl, url: url, mapUrl: mapUrl});
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
