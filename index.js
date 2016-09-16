61a57c0c2c57212309dfa939fa4a90ada8fd097f
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2016-09-16 15:28:49 -0400

    Restructure.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-#!/usr/bin/env node
-
 /**
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('where-should-i-eat:server');
 var http = require('http');
 
