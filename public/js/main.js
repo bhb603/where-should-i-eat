704b28f14cc2ca1b1b4f52e9fab0d87c4c3024ac
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2016-09-18 18:33:08 -0400

    Update styling.

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,4 @@
-function lazyLoadCss(filename) {
-    var l = document.createElement('link');
-    l.rel = 'stylesheet';
-    l.href = filename
-    var h = document.getElementsByTagName('head')[0];
-    h.parentNode.insertBefore(l, h);
-}
 window.addEventListener('load', function() {
-  lazyLoadCss('https://fonts.googleapis.com/css?family=Raleway');
   var clearEl = document.getElementById('clear');
   if (clearEl) {
     clearEl.onclick = function(event) {
